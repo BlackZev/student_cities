3a8a38ce8c751503493bec9b4d7f0436cfe6140d
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-05-11 16:54:48 +0200

    Add todelete controller to test routing

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        $companies = [
+            'Apple' => '$1.16 trillion USD',
+            'Samsung' => '$298.68 billion USD',
+            'Microsoft' => '$1.10 trillion USD',
+        ];
+
+        return $this->render('test/index.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+}
